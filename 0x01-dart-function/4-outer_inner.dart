64cea1e36e7de4958af9d87ad5466a85c8ca2a44
SoliraZ <48773711+SoliraZ@users.noreply.github.com>
2026-07-27 13:59:50 +0200

    Add a new Dart file to demonstrate outer and inner function usage with string manipulation

---
+++ 0x01-dart-function/4-outer_inner.dart
@@ -0,0 +1,10 @@
+void outer(String name, String id) {
+  String inner() {
+    List<String> parts = name.split(' ');
+    String firstName = parts[0];
+    String lastName = parts[1];
+    return 'Hello Agent ${lastName[0]}.$firstName your id is $id';
+  }
+
+  print(inner());
+}
